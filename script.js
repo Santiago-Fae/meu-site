444fc4e6b9b7b4409c9b7efd5472598adc495b64
Santiago <santiagopoffo@hotmail.com>
2021-04-08 01:40:26 -0300

    menu pronto

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 var node = document.getElementById("menu")
+
 var largura = window.innerWidth
 
 function removermenu() {
-if (node.parentNode && largura < 900) {
-    node.parentNode.removeChild(node);
-  }
+if (node.parentNode && largura < 1000) {
+    document.getElementById('check').click()
+}
 }
 
 
