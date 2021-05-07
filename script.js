49aba17fb7b444a559e6a2a250a75466ed3b371e
Santiago <santiagopoffo@hotmail.com>
2021-05-07 15:19:48 -0300

    tirando barrras

--- script.js
+++ script.js
@@ -9,16 +9,3 @@ if (node.parentNode && largura < 1000) {
 }
 }
 
-
-/* var nav = document.getElementById("menu");
-var largura = window.innerWidth
-
-function removermenu() {
-    if (largura < 900 ) {
-        nav.onclick() = function() {
-            this.remove()
-        }
-    }
-}
-
-removermenu() */
